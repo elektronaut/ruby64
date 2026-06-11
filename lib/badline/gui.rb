47aecd165620474b1df3c606661bdcaaf260fa36
Inge Jørgensen <inge@elektronaut.no>
2026-06-11 08:21:18 +0200

    badline executable

---
+++ lib/badline/gui.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require "sdl2"
+
+require "badline/gui/palette"
+require "badline/gui/key_map"
+require "badline/gui/joy_map"
+require "badline/gui/pane"
+require "badline/gui/screen_pane"
+require "badline/gui/window"
+require "badline/gui/application"
